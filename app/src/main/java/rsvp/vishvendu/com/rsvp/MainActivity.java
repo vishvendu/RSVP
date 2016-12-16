669d10db1dd4f983a54639ff8723bdf2b5e243d9
vishvendu.palawat <vishal.palawat785@gmail.com>
2016-12-16 16:44:14 +0530

    Myfirstcommitongit

---
+++ app/src/main/java/rsvp/vishvendu/com/rsvp/MainActivity.java
@@ -0,0 +1,19 @@
+package rsvp.vishvendu.com.rsvp;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView MobileNoText = (TextView) findViewById(R.id.mobilenotext);
+        Typeface face = Typeface.createFromAsset(getAssets(),
+                "fonts/sc_sans_app_thin.ttf");
+        MobileNoText.setTypeface(face);
+    }
+}

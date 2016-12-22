2944420ef9f24c021c38b43ca15ebd1132b23773
vishal.palawat785@gmail.com <Qazxsw@21>
2016-12-22 10:16:51 +0530

    Added flight fragment

--- app/src/main/java/rsvp/vishvendu/com/rsvp/Onboarding/Module/Fragment/FlightBookingDetails.java
+++ app/src/main/java/rsvp/vishvendu/com/rsvp/Onboarding/Module/Fragment/FlightBookingDetails.java
@@ -3,10 +3,17 @@ package rsvp.vishvendu.com.rsvp.Onboarding.Module.Fragment;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.transition.ChangeBounds;
+import android.transition.Slide;
+import android.transition.Transition;
+import android.transition.TransitionInflater;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import rsvp.vishvendu.com.rsvp.Onboarding.Module.Activity.OnBoardingMainActivity;
@@ -16,6 +23,9 @@ public class FlightBookingDetails extends Fragment implements View.OnClickListen
 
 TextView FlightBookingdetail;
     Button Proceed;
+    RelativeLayout myRelative;
+    FlightBookingDetails flightBookingDetails;
+    expanddateview expanddateview;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -29,7 +39,9 @@ TextView FlightBookingdetail;
 
         FlightBookingdetail=(TextView)view.findViewById(R.id.bookingbehalf);
         Proceed=(Button)view.findViewById(R.id.flightproceed);
+        myRelative = (RelativeLayout)view.findViewById(R.id.RRlayout);
         Proceed.setOnClickListener(this);
+        myRelative.setOnClickListener(this);
         Typeface face = Typeface.createFromAsset(getActivity().getAssets(),
                 "fonts/sc_sans_app_thin.ttf");
         FlightBookingdetail.setTypeface(face);
@@ -39,7 +51,34 @@ TextView FlightBookingdetail;
     @Override
     public void onClick(View v) {
 
-        OnBoardingMainActivity onBoardingMainActivity = (OnBoardingMainActivity)getActivity();
-        onBoardingMainActivity.initDocumentDetails();
+        Transition changeTransform = TransitionInflater.from(getActivity()).
+                inflateTransition(R.transition.change_image_transform);
+        Transition explodeTransform = TransitionInflater.from(getActivity()).
+                inflateTransition(android.R.transition.explode);
+
+        FlightBookingDetails flightBookingdetail =new FlightBookingDetails();
+
+        // Setup exit transition on first fragment
+        flightBookingdetail.setSharedElementReturnTransition(changeTransform);
+        flightBookingdetail.setExitTransition(explodeTransform);
+
+        expanddateview expanddateviewT = new expanddateview();
+        // Setup enter transition on second fragment
+        expanddateviewT.setSharedElementEnterTransition(changeTransform);
+        expanddateviewT.setEnterTransition(explodeTransform);
+
+        // Find the shared element (in Fragment A)
+        RelativeLayout ivProfile = (RelativeLayout) v.findViewById(R.id.RRlayout);
+
+        // Add second fragment by replacing first
+        FragmentTransaction ft = getFragmentManager().beginTransaction()
+                .replace(R.id.onboarding_container, expanddateviewT)
+                .addToBackStack("transaction")
+                .addSharedElement(ivProfile, "profile");
+        // Apply the transaction
+        ft.commit();
+
+        /*OnBoardingMainActivity onBoardingMainActivity = (OnBoardingMainActivity)getActivity();
+        onBoardingMainActivity.initDocumentDetails();*/
     }
 }
---
+++ app/src/main/java/rsvp/vishvendu/com/rsvp/Onboarding/Module/Fragment/expanddateview.java
@@ -0,0 +1,31 @@
+package rsvp.vishvendu.com.rsvp.Onboarding.Module.Fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import rsvp.vishvendu.com.rsvp.R;
+
+
+public class expanddateview extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_expanddateview, container, false);
+        return view;
+    }
+
+
+}
